9fd673d79dd83aaf268fd9b9c4fe7a2f485b6f68
Oleg Bovykin <oleg.bovykin@gmail.com>
2012-03-15 12:12:33 +0400

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Newsandmore::Application.config.secret_token = '70ab5846b90e62873b58b28f6d42b969cb208b1933d61b014e0ba173851b41a5229d6849a7664dd987b63b9350cd4f9f8bd5c990d8d2150a167e60d94ef7bc81'
