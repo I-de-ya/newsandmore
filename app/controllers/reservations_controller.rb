d308f530c9fcf6efc93d6a15b1efa73c7c494a5d
veskakoff <veskakov@gmai.com>
2012-03-19 00:44:35 +0400

    Small changes

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -44,7 +44,7 @@ class ReservationsController < ApplicationController
 
     respond_to do |format|
       if @reservation.save
-        format.html { redirect_to @reservation, notice: 'Reservation was successfully created.' }
+        format.html { redirect_to new_reservation_path, notice: 'Reservation was successfully created.' }
         format.json { render json: @reservation, status: :created, location: @reservation }
       else
         format.html { render action: "new" }
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,6 +2,7 @@ class Reservation < ActiveRecord::Base
 	email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :firstname, :lastname, :persons, :presence => true
 	validates :email, :format => { :with => email_regex }
+	validates :persons, :numericality => {:greater_than_or_equal_to => 1}
 end
 	
 	
