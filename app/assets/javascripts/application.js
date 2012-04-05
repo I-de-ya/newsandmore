cc797f20701a1bb62f19ffc658683ec506523bbc
veskakoff <veskakov@gmai.com>
2012-04-06 00:42:01 +0400

    Fixed section slider

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -78,33 +78,31 @@ $(document).ready(function(){
 
 // слайдер
 function theRotator() {
-	// Устанавливаем прозрачность всех картинок в 0
-	$('.slider ul li, .slider_title').css({opacity: 0.0});
- 
-	// Берем первую картинку и показываем ее (по пути включаем полную видимость)
-	$('.slider ul li:first, .slider_title:first').css({opacity: 1.0}).addClass('show');
- 
-	// Вызываем функцию rotate для запуска слайдшоу, 5000 = смена картинок происходит раз в 5 секунд
-	setInterval('rotate()',7000);
+
+	$('.slider ul li, .slider_title li, .section_images ul li').css({opacity: 0.0});
+	$('.slider ul li:first, .slider_title li:first, .section_images ul li:first').css({opacity: 1.0}).addClass('show');
+    
+    
+	
+    if($('.section_images ul li').size() > 1 || $('.slider ul li').size() > 1) {
+        setInterval('rotate()',7000);
+    }
 }
  
 function rotate() {	
-	// Берем первую картинку
-	var current = ($('.slider ul li.show, .slider_title.show')?  $('div.slider ul li.show, .slider_title.show') : $('.slider ul li:first, .slider_title:first'));
- 
-	// Берем следующую картинку, когда дойдем до последней начинаем с начала
-	var next = ((current.next().length - 1) ? ((current.next().hasClass('show')) ? $('.slider ul li:first, .slider_title:first') :current.next()) : $('.slider ul li:first, .slider_title:first'));	
+	var current = ($('.slider ul li.show, .slider_title li.show, .section_images ul li.show')?  $('.slider ul li.show, .slider_title li.show, .section_images ul li.show') : $('.slider ul li:first, .slider_title li:first, .section_images ul li:first'));
+
+	var next = ((current.next().length) ? ((current.next().hasClass('show')) ? $('.slider ul li:first, .slider_title li:first, .section_images ul li:first') :current.next()) : $('.slider ul li:first, .slider_title li:first, .section_images ul li:first'));	
+
  
 	next.css({opacity: 0.0})
 	.addClass('show')
 	.animate({opacity: 1.0}, 3000);
  
-	// Прячем текущую картинку
 	current.animate({opacity: 0.0}, 3000)
 	.removeClass('show');
 };
  
 $(document).ready(function() {		
-	// Запускаем слайдшоу
 	theRotator();
 });
