ac9c19d7ad65d33a145b5d1ec678903ae19a3a69
inner-whisper <maks.rydkin@gmail.com>
2012-04-11 02:13:32 +0400

    Added translations for attributes

--- app/controllers/admin/messages_controller.rb
+++ app/controllers/admin/messages_controller.rb
@@ -1,59 +1,14 @@
 #coding: utf-8
 class Admin::MessagesController < Admin::ApplicationController
-  def toggleshow
-    @message = Message.find(params[:id])
-    @message.toggle(:visible)
-    @message.save
-    redirect_to :back, notice: 'Сообщение обновлено.'
-  end
 
   def index
     @page_title = "Обратная связь"
     @messages = Message.all
   end
 
-  def new
-    @message = Message.new
-    render "edit"
-  end
-
-  def edit
-    @page_title = "Редактирование сообщения"    
-    @message = Message.find(params[:id])
-  end
-
-  def create
-    @message = Message.new(params[:message])
-
-    respond_to do |format|
-      if @message.save
-        format.html { redirect_to (params[:commit] == "Сохранить" ? [:admin,:messages] : [:edit,:admin,@message]), notice: 'Сообщение успешно добавлен.' }
-        format.json { render json: @message, status: :created, location: @message }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @message.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /messages/1
-  # PUT /messages/1.json
-  def update
-    @message = Message.find(params[:id])
-
-    respond_to do |format|
-      if @message.update_attributes(params[:message])
-        format.html { redirect_to (params[:commit] == "Сохранить" ? [:admin,:messages] : [:edit,:admin,@message]), notice: 'Фильм успешно обновлен.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @message.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
   def show
-    
+    @page_title = "Редактирование сообщения"
+    @message = Message.find(params[:id])
   end
 
   def destroy
